7fae4eec557a56764bcfeb56ea4fe9b4cc780111
yogibear24 <yogibear24@users.noreply.github.com>
2020-03-07 13:17:26 -0800

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -1,3 +1,5 @@
+import random
+
 def create_deck_of_cards():
     list_of_suits = ["Red_Heart", "Red_Diamond", "Black_Spade", "Black_Clover"]
     list_of_card_types = [str(number) for number in range(1, 11)] + ["Jack", "Queen", "King"]
@@ -16,14 +18,34 @@ def assign_card_values_to_dict(list_of_all_cards):
         if current_card[-1].isnumeric() is True and current_card[-1] != "0":
             card_value_dict.update({current_card : int(current_card[-1])})
         # Assign initial value of 1 to a 1 Card, but later on mechanic is it can be 1 or 11...
-        elif current_card.endswith("10"):
-            card_value_dict.update({current_card: 10})
-        elif current_card.endswith("Jack"):
-            card_value_dict.update({current_card : 11})
-        elif current_card.endswith("Queen"):
-            card_value_dict.update({current_card : 12})
-        elif current_card.endswith("King"):
-            card_value_dict.update({current_card : 13})
+        elif current_card.endswith(("10", "Jack", "Queen", "King")):
+            card_value_dict.update({current_card : 10})
     return(card_value_dict)
 
 assigned_card_values = assign_card_values_to_dict(list_of_all_cards)
+
+def deal_initial_cards(list_of_all_cards):
+    drawn_cards = random.sample(list_of_all_cards, 4)
+    dealer_hand = drawn_cards[:2]
+    player_hand = drawn_cards[2:]
+    return(dealer_hand, player_hand)
+
+dealer_hand, player_hand = deal_initial_cards(list_of_all_cards)
+
+def output_dealt_initial_cards(dealer_hand, player_hand, assigned_card_values):
+    print("The Dealer has a " + dealer_hand[0].split(" ")[-1] + " and one Unknown Card")
+    print("You have a " + player_hand[0].split(" ")[-1] + " and a " + player_hand[1].split(" ")[-1])
+    player_initial_total = assigned_card_values[player_hand[0]] + assigned_card_values[player_hand[1]]
+    print("\n" + "The Dealer currently has " + str(assigned_card_values[dealer_hand[0]]) + " and one Unknown Number")
+    # Print the dealer's hand value in case people do not know what value a face card is
+    print("Your current total is " + str(player_initial_total))
+
+output_dealt_initial_cards(dealer_hand, player_hand, assigned_card_values)
+
+def prompt_to_hit():
+    print("\n" + "Do you want to Hit?")
+    print("Type 'Y' to Hit or 'N' to Stay")
+    hit_input = input()
+    return(hit_input)
+
+hit_input = prompt_to_hit()
