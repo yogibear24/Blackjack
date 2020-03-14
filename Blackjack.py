4e66edbba9982f7587a97d94c29f34e93257044e
yogibear24 <yogibear24@users.noreply.github.com>
2020-03-14 00:12:55 -0700

    Add files via upload

--- Blackjack.py
+++ Blackjack.py
@@ -71,13 +71,13 @@ def prompt_to_hit():
 
 hit_input = prompt_to_hit()
 
-def draw_card(cards_left, player_hand, dealer_hand, list_of_all_cards):
-    player_hand.append(random.sample(cards_left, 1)[0])
+def draw_card(cards_left, hand_to_append, other_hand):
+    hand_to_append.append(random.sample(cards_left, 1)[0])
     # random.sample returns a list
-    return([card for card in list_of_all_cards if card not in player_hand + dealer_hand])
+    return([card for card in cards_left if card not in hand_to_append + other_hand])
 
-def output_dealer_and_initial_status(assigned_card_values, dealer_hand, player_hand):
-    print("\n" + "The Dealer currently has " + str(assigned_card_values[dealer_hand[0]]) + " and one Unknown Number")
+def output_dealer_and_initial_status(dealer_hand, player_hand):
+    print("\n" + "The Dealer has a " + dealer_hand[0].split(" ")[-1] + " and one Unknown Card")
     initial_string = ("You have a " + player_hand[0].split(" ")[-1] + ", a " + player_hand[1].split(" ")[-1])
     for card in range(2, len(player_hand) - 1):
         initial_string += (", a " + player_hand[card].split(" ")[-1])
@@ -90,13 +90,11 @@ def action_output(original_list_of_totals):
     elif len(original_list_of_totals) == 1:
         print("Your current total is " + str(original_list_of_totals).strip("[]"))
 
-def output_if_stay_or_incorrect_input(assigned_card_values, dealer_hand, player_hand, list_of_possible_player_initial_totals, new_player_totals):
+def output_if_stay_or_incorrect_input(dealer_hand, player_hand):
     if len(player_hand) == 2:
-        output_dealer_and_initial_status(assigned_card_values, dealer_hand, player_hand)
-        action_output(list_of_possible_player_initial_totals)
+        output_dealer_and_initial_status(dealer_hand, player_hand)
     elif len(player_hand) > 2:
-        output_dealer_and_initial_status(assigned_card_values, dealer_hand, player_hand)
-        action_output(new_player_totals)
+        output_dealer_and_initial_status(dealer_hand, player_hand)
 
 def check_for_21_or_over(new_player_totals, update_counter, player_hand, cards_left):
     if all(total > 21 for total in new_player_totals) is True:
@@ -107,69 +105,128 @@ def check_for_21_or_over(new_player_totals, update_counter, player_hand, cards_l
     else:
         hit_input = prompt_to_hit()
 
-def bust_output(new_player_totals, update_counter):
-    new_player_totals = ["BUST"]
+def bust_output(final_player_value, update_counter):
+    final_player_value = "BUST"
     print("\n" + "BUST... Got over 21, moving to Dealer's Turn")
     update_counter += 1
 
 def update_player_hand(hit_input, player_hand, dealer_hand, cards_left, assigned_card_values, list_of_possible_player_initial_totals):
     update_counter = 0
     new_player_totals = []
+    final_player_value = 0
     while update_counter < 1:
         if hit_input == "Y" and len(player_hand) == 2:
-            cards_left = draw_card(cards_left, player_hand, dealer_hand, list_of_all_cards)
-            output_dealer_and_initial_status(assigned_card_values, dealer_hand, player_hand)
+            cards_left = draw_card(cards_left, player_hand, dealer_hand)
+            output_dealer_and_initial_status(dealer_hand, player_hand)
             if player_hand[-1].endswith("Ace"):
                 new_player_totals = ([entry + 1 for entry in list_of_possible_player_initial_totals]
                                      + [entry + 11 for entry in list_of_possible_player_initial_totals])
                 action_output(new_player_totals)
                 if all(total > 21 for total in new_player_totals) is True:
-                    bust_output(new_player_totals, update_counter)
-                    return (player_hand, cards_left, new_player_totals)
+                    bust_output(final_player_value, update_counter)
+                    return (player_hand, cards_left, final_player_value)
                 else:
                     hit_input = prompt_to_hit()
             else:
                 new_player_totals = [entry + assigned_card_values[player_hand[2]] for entry in list_of_possible_player_initial_totals]
                 action_output(new_player_totals)
                 if all(total > 21 for total in new_player_totals) is True:
-                    bust_output(new_player_totals, update_counter)
-                    return (player_hand, cards_left, new_player_totals)
+                    bust_output(final_player_value, update_counter)
+                    return (player_hand, cards_left, final_player_value)
                 else:
                     hit_input = prompt_to_hit()
         elif hit_input == "Y" and len(player_hand) > 2:
-            cards_left = draw_card(cards_left, player_hand, dealer_hand, list_of_all_cards)
-            output_dealer_and_initial_status(assigned_card_values, dealer_hand, player_hand)
+            cards_left = draw_card(cards_left, player_hand, dealer_hand)
+            output_dealer_and_initial_status(dealer_hand, player_hand)
             if player_hand[-1].endswith("Ace"):
                 new_player_totals = [entry + 1 for entry in new_player_totals] + [entry + 11 for entry in new_player_totals]
                 action_output(new_player_totals)
                 if all(total > 21 for total in new_player_totals) is True:
-                    bust_output(new_player_totals, update_counter)
-                    return (player_hand, cards_left, new_player_totals)
+                    bust_output(final_player_value, update_counter)
+                    return (player_hand, cards_left, final_player_value)
                 else:
                     hit_input = prompt_to_hit()
             else:
                 new_player_totals = [entry + assigned_card_values[player_hand[-1]] for entry in new_player_totals]
                 action_output(new_player_totals)
                 if all(total > 21 for total in new_player_totals) is True:
-                    bust_output(new_player_totals, update_counter)
-                    return (player_hand, cards_left, new_player_totals)
+                    bust_output(final_player_value, update_counter)
+                    return (player_hand, cards_left, final_player_value)
                 else:
                     hit_input = prompt_to_hit()
         elif hit_input == "N":
             print("\n" + "Player chose to Stay")
             update_counter += 1
-            output_if_stay_or_incorrect_input(assigned_card_values, dealer_hand, player_hand,
-                                            list_of_possible_player_initial_totals, new_player_totals)
-            return (player_hand, cards_left, new_player_totals)
+            output_if_stay_or_incorrect_input(dealer_hand, player_hand)
+            if new_player_totals == []:
+                new_player_totals = list_of_possible_player_initial_totals
+                final_player_value = max([total for total in new_player_totals if total <= 21])
+                print("Your Total is " + str(final_player_value))
+                return (player_hand, cards_left, final_player_value)
+            else:
+                final_player_value = max([total for total in new_player_totals if total <= 21])
+                print("Your Total is " + str(final_player_value))
+                return (player_hand, cards_left, final_player_value)
         else:
             print("\n" + "Incorrect input...")
-            output_if_stay_or_incorrect_input(assigned_card_values, dealer_hand, player_hand,
-                                            list_of_possible_player_initial_totals, new_player_totals)
+            output_if_stay_or_incorrect_input(dealer_hand, player_hand)
             hit_input = prompt_to_hit()
 
-player_hand, cards_left, new_player_totals = update_player_hand(hit_input, player_hand, dealer_hand, cards_left,
+player_hand, cards_left, final_player_value = update_player_hand(hit_input, player_hand, dealer_hand, cards_left,
                                                                 assigned_card_values,
                                                                 list_of_possible_player_initial_totals)
 
-# TODO: Create function for dealer to show Unknown Card and draw until 17 to 21 or bust
-# TODO: Calculation function to see who wins
+def generate_possible_dealer_initial_totals(dealer_hand, assigned_card_values, list_of_possible_dealer_initial_totals):
+    if assigned_card_values[dealer_hand[0]] == 1 and assigned_card_values[dealer_hand[1]] != 1:
+        list_of_possible_dealer_initial_totals.append(1 + assigned_card_values[dealer_hand[1]])
+        list_of_possible_dealer_initial_totals.append(11 + assigned_card_values[dealer_hand[1]])
+    elif assigned_card_values[dealer_hand[0]] == 1 and assigned_card_values[dealer_hand[1]] == 1:
+        list_of_possible_dealer_initial_totals.append(1 + 1)
+        list_of_possible_dealer_initial_totals.append(11 + 1)
+        list_of_possible_dealer_initial_totals.append(11 + 11)
+    elif assigned_card_values[dealer_hand[0]] != 1 and assigned_card_values[dealer_hand[1]] == 1:
+        list_of_possible_dealer_initial_totals.append(assigned_card_values[dealer_hand[0]] + 1)
+        list_of_possible_dealer_initial_totals.append(assigned_card_values[dealer_hand[0]] + 11)
+    elif assigned_card_values[dealer_hand[0]] != 1 and assigned_card_values[dealer_hand[1]] != 1:
+        list_of_possible_dealer_initial_totals.append(assigned_card_values[dealer_hand[0]] + assigned_card_values[dealer_hand[1]])
+
+def output_dealer_status(dealer_hand):
+    initial_string = ("The Dealer has a " + dealer_hand[0].split(" ")[-1] + ", a " + dealer_hand[1].split(" ")[-1])
+    for card in range(2, len(dealer_hand) - 1):
+        initial_string += (", a " + dealer_hand[card].split(" ")[-1])
+    initial_string += (", and a " + dealer_hand[-1].split(" ")[-1])
+    print(initial_string)
+
+def update_dealer_hand(player_hand, cards_left, dealer_hand, assigned_card_values):
+    print("\n" + "The Dealer flips over the Unknown Card")
+    print("The Dealer has a " + dealer_hand[0].split(" ")[-1] + " and a " + dealer_hand[1].split(" ")[-1])
+    list_of_possible_dealer_totals = []
+    generate_possible_dealer_initial_totals(dealer_hand, assigned_card_values, list_of_possible_dealer_totals)
+    dealer_final_value = 0
+    dealer_counter = 0
+    while dealer_counter < 1:
+        if all(total < 17 for total in list_of_possible_dealer_totals) is True:
+            print("\n" + "The Dealer draws a card...")
+            cards_left = draw_card(cards_left, dealer_hand, player_hand)
+            output_dealer_status(dealer_hand)
+            if dealer_hand[-1].endswith("Ace"):
+                list_of_possible_dealer_totals = ([entry + 1 for entry in list_of_possible_dealer_totals]
+                                                  + [entry + 11 for entry in list_of_possible_dealer_totals])
+            else:
+                list_of_possible_dealer_totals = [entry + assigned_card_values[dealer_hand[-1]] for entry in list_of_possible_dealer_totals]
+        elif all(total > 21 for total in list_of_possible_dealer_totals) is True:
+            dealer_counter += 1
+            dealer_final_value = "BUST"
+            print("\n" + "BUST... Dealer got over 21")
+            return(dealer_final_value)
+        elif any((17 <= total <= 21) for total in list_of_possible_dealer_totals) is True:
+            dealer_counter += 1
+            list_of_possible_dealer_totals = [total for total in list_of_possible_dealer_totals if (17 <= total <= 21)]
+            dealer_final_value = max(list_of_possible_dealer_totals)
+            print("The Dealer Total is " + str(dealer_final_value))
+            return(dealer_final_value)
+
+dealer_final_value = update_dealer_hand(player_hand, cards_left, dealer_hand, assigned_card_values)
+
+# TODO: Calculation function to see who wins using player_final_value and dealer_final_value
+# TODO: Loop and Prompt Menu for player to continue playing or not
